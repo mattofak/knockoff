6776882546ac46bea72ddd4689d26d813b85d63b
Gabriel Wicke <wicke@wikidev.net>
2014-03-14 13:44:05 -0700

    Document the KnockOff API

--- knockoff.js
+++ knockoff.js
@@ -1,3 +1,4 @@
+"use strict";
 var TAssembly = require('tassembly').TAssembly,
 	koCompiler = require('./KnockoutCompiler.js');
 
@@ -5,11 +6,25 @@ function KnockOff () {
 	this.TA = new TAssembly();
 }
 
-KnockOff.prototype.compile = function(template, cb) {
-	var templateASM = koCompiler.compile(template);
-	return this.TA.compile(templateASM, cb);
+/**
+ * Compile a Knockout template to a function
+ *
+ * @param {misc} template HTML string or DOM node
+ * @param {object} options
+ * @returns {function(model)} function that can be called with a model and
+ *							  returns an HTML string
+ */
+KnockOff.prototype.compile = function(template, options) {
+	var templateASM = koCompiler.compile(template, options);
+	return this.TA.compile(templateASM, options && options.cb);
 };
 
+/**
+ * Register a partial (nested template)
+ *
+ * @param {string} name of the template
+ * @param {misc} template HTML string or DOM node
+ **/
 KnockOff.prototype.registerPartial = function(name, template) {
 	this.TA.partials[name] = koCompiler.compile(template);
 };
