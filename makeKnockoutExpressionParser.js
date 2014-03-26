d17da091c51364da02a426d802b78dffd61a2512
Gabriel Wicke <wicke@wikidev.net>
2014-03-25 17:07:46 -0700

    Try space-optimized parser instead

--- makeKnockoutExpressionParser.js
+++ makeKnockoutExpressionParser.js
@@ -5,7 +5,7 @@
 var PEG = require('pegjs'),
 	fs = require('fs'),
 	grammar = fs.readFileSync('./KnockoutExpressionParser.pegjs', 'utf8'),
-	parser = PEG.buildParser(grammar, {output:"source"});
+	parser = PEG.buildParser(grammar, {output:"source", optimize:"size"});
 
 console.log('Re-building KnockoutExpressionParser.js '
 		+ 'from KnockoutExpressionParser.pegjs');
